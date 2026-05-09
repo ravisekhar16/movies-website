d2eabaa2a176df17fa86da7625add60205e9543a
ravisekhar16 <ravisekhar029@gmail.com>
2026-05-09 17:35:00 +0530

    fixed vercel base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base:"/movies-website/",
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:"/Movies-website/",
+  base: "/",
   plugins: [react()],
 })
